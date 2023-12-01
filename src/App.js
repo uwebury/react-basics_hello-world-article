dba2993874ddc0c0a4ca49074287485ba1299c23
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-01 12:34:16 +0100

    Created HelloWorldArticle component

--- src/App.js
+++ src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Heading</h1>
+      <p>Paragraph</p>
+    </article>
+  );
 }
